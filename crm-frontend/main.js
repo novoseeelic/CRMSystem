847e6c53f77dbdabf19eec368b51f627f77ce144
Valeria Sizinkevich <leranovoselic@gmail.com>
2024-09-13 14:29:25 +0300

    added folders frontend and backend

---
+++ crm-frontend/main.js
@@ -0,0 +1,424 @@
+let svgs = {
+  fb: `<svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <g opacity="0.7">
+        <path d="M7.99999 0C3.6 0 0 3.60643 0 8.04819C0 12.0643 2.928 15.3976 6.75199 16V10.3775H4.71999V8.04819H6.75199V6.27309C6.75199 4.25703 7.94399 3.14859 9.77599 3.14859C10.648 3.14859 11.56 3.30121 11.56 3.30121V5.28514H10.552C9.55999 5.28514 9.24799 5.90362 9.24799 6.53815V8.04819H11.472L11.112 10.3775H9.24799V16C11.1331 15.7011 12.8497 14.7354 14.0879 13.2772C15.3261 11.819 16.0043 9.96437 16 8.04819C16 3.60643 12.4 0 7.99999 0Z" fill="#9873FF"/>
+        </g>
+      </svg>`,
+  email: `<svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path opacity="0.7" fill-rule="evenodd" clip-rule="evenodd" d="M8 16C12.4183 16 16 12.4183 16 8C16 3.58172 12.4183 0 8 0C3.58172 0 0 3.58172 0 8C0 12.4183 3.58172 16 8 16ZM4 5.75C4 5.3375 4.36 5 4.8 5H11.2C11.64 5 12 5.3375 12 5.75V10.25C12 10.6625 11.64 11 11.2 11H4.8C4.36 11 4 10.6625 4 10.25V5.75ZM8.424 8.1275L11.04 6.59375C11.14 6.53375 11.2 6.4325 11.2 6.32375C11.2 6.0725 10.908 5.9225 10.68 6.05375L8 7.625L5.32 6.05375C5.092 5.9225 4.8 6.0725 4.8 6.32375C4.8 6.4325 4.86 6.53375 4.96 6.59375L7.576 8.1275C7.836 8.28125 8.164 8.28125 8.424 8.1275Z" fill="#9873FF"/>
+          </svg>`,
+  tel: `<svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <g opacity="0.7">
+          <circle cx="8" cy="8" r="8" fill="#9873FF"/>
+          <path d="M11.56 9.50222C11.0133 9.50222 10.4844 9.41333 9.99111 9.25333C9.83556 9.2 9.66222 9.24 9.54222 9.36L8.84444 10.2356C7.58667 9.63556 6.40889 8.50222 5.78222 7.2L6.64889 6.46222C6.76889 6.33778 6.80444 6.16444 6.75556 6.00889C6.59111 5.51556 6.50667 4.98667 6.50667 4.44C6.50667 4.2 6.30667 4 6.06667 4H4.52889C4.28889 4 4 4.10667 4 4.44C4 8.56889 7.43556 12 11.56 12C11.8756 12 12 11.72 12 11.4756V9.94222C12 9.70222 11.8 9.50222 11.56 9.50222Z" fill="white"/>
+          </g>
+        </svg>`,
+  vk: `<svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <path d="M8 0C3.58187 0 0 3.58171 0 8C0 12.4183 3.58187 16 8 16C12.4181 16 16 12.4183 16 8C16 3.58171 12.4181 0 8 0ZM12.058 8.86523C12.4309 9.22942 12.8254 9.57217 13.1601 9.97402C13.3084 10.1518 13.4482 10.3356 13.5546 10.5423C13.7065 10.8371 13.5693 11.1604 13.3055 11.1779L11.6665 11.1776C11.2432 11.2126 10.9064 11.0419 10.6224 10.7525C10.3957 10.5219 10.1853 10.2755 9.96698 10.037C9.87777 9.93915 9.78382 9.847 9.67186 9.77449C9.44843 9.62914 9.2543 9.67366 9.1263 9.90707C8.99585 10.1446 8.96606 10.4078 8.95362 10.6721C8.93577 11.0586 8.81923 11.1596 8.43147 11.1777C7.60291 11.2165 6.81674 11.0908 6.08606 10.6731C5.44147 10.3047 4.94257 9.78463 4.50783 9.19587C3.66126 8.04812 3.01291 6.78842 2.43036 5.49254C2.29925 5.2007 2.39517 5.04454 2.71714 5.03849C3.25205 5.02817 3.78697 5.02948 4.32188 5.03799C4.53958 5.04143 4.68362 5.166 4.76726 5.37142C5.05633 6.08262 5.4107 6.75928 5.85477 7.38684C5.97311 7.55396 6.09391 7.72059 6.26594 7.83861C6.45582 7.9689 6.60051 7.92585 6.69005 7.71388C6.74734 7.57917 6.77205 7.43513 6.78449 7.29076C6.82705 6.79628 6.83212 6.30195 6.75847 5.80943C6.71263 5.50122 6.53929 5.30218 6.23206 5.24391C6.07558 5.21428 6.0985 5.15634 6.17461 5.06697C6.3067 4.91245 6.43045 4.81686 6.67777 4.81686L8.52951 4.81653C8.82136 4.87382 8.88683 5.00477 8.92645 5.29874L8.92808 7.35656C8.92464 7.47032 8.98521 7.80751 9.18948 7.88198C9.35317 7.936 9.4612 7.80473 9.55908 7.70112C10.0032 7.22987 10.3195 6.67368 10.6029 6.09801C10.7279 5.84413 10.8358 5.58142 10.9406 5.31822C11.0185 5.1236 11.1396 5.02785 11.3593 5.03112L13.1424 5.03325C13.195 5.03325 13.2483 5.03374 13.3004 5.04274C13.6009 5.09414 13.6832 5.22345 13.5903 5.5166C13.4439 5.97721 13.1596 6.36088 12.8817 6.74553C12.5838 7.15736 12.2661 7.55478 11.9711 7.96841C11.7001 8.34652 11.7215 8.53688 12.058 8.86523Z" fill="#9873FF"/>
+  </svg>
+  `
+}
+
+const clients = [
+  {id: 123455,
+  name: "Денис",
+  surname: "Скворцов",
+  lastname: "Юрьевич",
+  dateOfCreation: new Date(2021, 02, 21, 12, 41),
+  lastModified: new Date(2021, 02, 21, 12, 41),
+  contacts: [
+    {type: "Вконтакте",
+    value: "про"},
+    {type: "Телефон",
+    value: "+7 (967) 890 78 90"},
+  ]},
+  {id: 123456,
+  name: "Арсений",
+  surname: "Куприянов",
+  lastname: "Валерьевич",
+  dateOfCreation: new Date(2021, 01, 28, 16, 40),
+  lastModified: new Date(2021, 02, 02, 17, 01),
+  contacts: [
+    {type: "Вконтакте",
+    value: "про"},
+    {type: "Телефон",
+    value: "+7 (967) 890 78 90"},
+  ]},
+  {id: 123457,
+  name: "Людмила",
+  surname: "Константинопольская",
+  lastname: "Александровна",
+  dateOfCreation: new Date(2021, 01, 20, 14, 01),
+  lastModified: new Date(2021, 01, 21, 14, 59),
+  contacts: [
+    {type: "Вконтакте",
+    value: "про"},
+    {type: "Телефон",
+    value: "+7 (967) 890 78 90"},
+  ]},
+  {id: 123458,
+  name: "Олег",
+  surname: "Дмитриевский",
+  lastname: "Алексеевич",
+  dateOfCreation: new Date(2021, 01, 14, 16, 50),
+  lastModified: new Date(2021, 01, 18, 16, 55),
+  contacts: [
+    {type: "Вконтакте",
+    value: "про"},
+    {type: "Телефон",
+    value: "+7 (967) 890 78 90"},
+  ]},
+  {id: 123459,
+  name: "Татьяна",
+  surname: "Александрова",
+  lastname: "Павловна",
+  dateOfCreation: new Date(2021, 01, 11, 12, 45),
+  lastModified: new Date(2021, 01, 11, 14, 11),
+  contacts: [
+    {type: "Вконтакте",
+    value: "про"},
+    {type: "Телефон",
+    value: "+7 (967) 890 78 90"},
+    {type: "Facebook",
+    value: "tyfhg"},
+    {type: "Email",
+    value: "test@test.test"},
+  ]}
+];
+
+const clientsList = document.getElementById("clients-list"),
+studentsListTHALL = document.querySelectorAll(".clientsTable th");
+
+function createContact(contactObj) {
+  let type;
+  switch (contactObj.type) {
+    case 'Email':
+      type = 'email';
+      break;
+    case 'Телефон':
+      type = 'tel';
+      break;
+    case 'Facebook':
+      type = 'fb';
+      break;
+    case 'Вконтакте':
+      type = 'vk';
+      break
+  }
+  const triggerWrapper = document.createElement('span');
+  const contactBtn = document.createElement('button');
+  contactBtn.innerHTML = svgs[type];
+  contactBtn.setAttribute('aria-expanded', false);
+  contactBtn.classList.add("btn-contact");
+  
+  const tippyinst = tippy(contactBtn, {
+    content: `<span> <a style="color: white"><b >${contactObj.value}</b></a></span>`,
+    allowHTML: true,
+    interactive: true,
+    appendTo:triggerWrapper,
+  })
+
+  tippyinst.popper.querySelector('a').addEventListener('blur', ()=>tippyinst.hide())
+  
+  triggerWrapper.append(contactBtn)
+  return triggerWrapper;
+}
+
+function createMoreBtn(num) {
+  const moreBtn = document.createElement('button');
+  moreBtn.textContent = `+${num}`;
+  return moreBtn;
+}
+
+// проверяем массив и в зависимости от количества добавляем кнопку эту функцию можно вызвать при каждой переросовке этого блока, добавлении нового контакта или изменениях.
+function checkContactsLength(contactsElems) {
+   if(contactsElems.length > 4) {
+    const hiddenContacts = contactsElems.slice(4)
+    hiddenContacts.forEach(item=>item.classList.add('hide'));
+    const moreBtn = createMoreBtn(hiddenContacts.length)
+    document.body.append(moreBtn);
+    moreBtn.addEventListener('click', ()=>{
+      hiddenContacts.forEach(item=>item.classList.remove('hide'));
+      moreBtn.remove()
+    })
+  }
+}
+
+function addBtns(contacts) {
+  // const contactsElems = contacts.map((contact) => createContact(contact))
+  // document.body.append(...contactsElems);
+  // checkContactsLength(contactsElems)
+
+  const contactsElems = createContact(contacts)
+  return contactsElems;
+}
+
+function newClientTR (clients) {
+  const clientTR = document.createElement("tr"),
+  idTD = document.createElement("td"),
+  fioTD = document.createElement("td"),
+  dateOfCreationTD = document.createElement("td"),
+  lastModifiedTD = document.createElement("td"),
+  contactTD = document.createElement("td"),
+  actionsTD = document.createElement("td")
+
+  idTD.textContent = clients.id;
+  idTD.classList.add("id");
+  fioTD.textContent = getFIO(clients);
+  dateOfCreationTD.textContent = getDateOfCreationString(clients);
+  lastModifiedTD.textContent = getLastModifiedString(clients);
+  
+  for(contact of clients.contacts) {
+    contactTD.append(addBtns(contact));
+  }
+  
+  let buttonGroup = document.createElement('div');
+  let changeButton = document.createElement('button');
+  let deleteButton = document.createElement('button');
+
+  changeButton.addEventListener("click", (e) => {
+    modal.classList.add("open");
+    createNewClient(clients);
+  })
+
+  const changeBtnSvg = `<svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <path d="M0 9.5002V12.0002H2.5L9.87333 4.62687L7.37333 2.12687L0 9.5002ZM11.8067 2.69354C12.0667 2.43354 12.0667 2.01354 11.8067 1.75354L10.2467 0.193535C9.98667 -0.0664648 9.56667 -0.0664648 9.30667 0.193535L8.08667 1.41354L10.5867 3.91354L11.8067 2.69354Z" fill="#9873FF"/>
+  </svg>
+  `,
+  deleteBtnSvg = `<svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <path d="M6 0C2.682 0 0 2.682 0 6C0 9.318 2.682 12 6 12C9.318 12 12 9.318 12 6C12 2.682 9.318 0 6 0ZM6 10.8C3.354 10.8 1.2 8.646 1.2 6C1.2 3.354 3.354 1.2 6 1.2C8.646 1.2 10.8 3.354 10.8 6C10.8 8.646 8.646 10.8 6 10.8ZM8.154 3L6 5.154L3.846 3L3 3.846L5.154 6L3 8.154L3.846 9L6 6.846L8.154 9L9 8.154L6.846 6L9 3.846L8.154 3Z" fill="#F06A4D"/>
+  </svg>  
+  `;
+  
+  changeButton.classList.add("btn-reset")
+  changeButton.innerHTML = changeBtnSvg + "Изменить";
+  deleteButton.classList.add("btn-reset")
+  deleteButton.innerHTML = deleteBtnSvg + "Удалить";
+
+  clientTR.append(idTD)
+  clientTR.append(fioTD)
+  clientTR.append(dateOfCreationTD)
+  clientTR.append(lastModifiedTD)
+  clientTR.append(contactTD)
+  buttonGroup.append(changeButton)
+  buttonGroup.append(deleteButton)
+  actionsTD.append(buttonGroup)
+  clientTR.append(actionsTD)
+
+  return clientTR;
+}
+
+function getFIO(clients) {
+  return clients.surname +" "+ clients.name +" "+ clients.lastname;
+}
+
+function getDateOfCreationString(clients) {
+  const yyyy = clients.dateOfCreation.getFullYear();
+  let mm = clients.dateOfCreation.getMonth() + 1;
+  let dd = clients.dateOfCreation.getDate();
+  let hh = clients.dateOfCreation.getHours();
+  let min = clients.dateOfCreation.getMinutes();
+  if(dd<10) dd = "0" + dd;
+  if(mm<10) mm = "0" + mm;
+  if(hh<10) hh = "0" + hh;
+  if(min<10) min = "0" + min;
+
+  const today = dd+"."+mm+"."+yyyy+" "+hh+":"+min;
+
+  return today;
+}
+
+function getLastModifiedString(clients) {
+  const yyyy = clients.lastModified.getFullYear();
+  let mm = clients.lastModified.getMonth() + 1;
+  let dd = clients.lastModified.getDate();
+  let hh = clients.lastModified.getHours();
+  let min = clients.lastModified.getMinutes();
+  if(dd<10) dd = "0" + dd;
+  if(mm<10) mm = "0" + mm;
+  if(hh<10) hh = "0" + hh;
+  if(min<10) min = "0" + min;
+
+  const today = dd+"."+mm+"."+yyyy+" "+hh+":"+min;
+
+  return today;
+}
+
+let clientsCopy = [...clients];
+
+for (const client of clientsCopy) {
+  clientsList.append(newClientTR(client));
+}
+
+let column = "fio";
+let columnDir = true;
+
+function getSortClients(prop, dir) {
+  const clientsCopy = [...clients];
+  if (prop == "fio") {
+    return clientsCopy.sort(function(clientA, clientB) {
+      if ((!dir == false ? getFIO(clientA) < getFIO(clientB) : getFIO(clientA) > getFIO(clientB)))
+      return -1
+    })
+  } else {
+    return clientsCopy.sort(function(clientA, clientB) {
+      if ((!dir == false ? clientA[prop] < clientB[prop] : clientA[prop] > clientB[prop]))
+      return -1
+    })
+  }
+}
+
+function render() {
+  let clientsCopy = [...clients];
+
+  clientsCopy = getSortClients(column, columnDir);
+
+  clientsList.innerHTML = "";
+  for (const client of clientsCopy) {
+    clientsList.append(newClientTR(client));
+  }
+}
+
+studentsListTHALL.forEach(element => {
+  element.addEventListener("click", function() {
+  column = this.dataset.column;
+  columnDir = !columnDir;
+  render();
+  })
+});
+
+function interlinearFilter(objects, key, value) {
+  const result = [];
+  if (key == "dateCreate" || key == "dateChange") {
+    for (let i = 0; i < objects.length; i++) {
+      if (getDate(objects[i][key]) == value) {
+        result.push(objects[i]);
+      }
+    }
+  } else if (key == "id"){
+      for (let i = 0; i < objects.length; i++) {
+        if (objects[i][key] == value) {
+          result.push(objects[i]);
+        }
+      }
+  } else {
+      for (let i = 0; i < objects.length; i++) {
+        if (objects[i][key].toLowerCase().search(value.toLowerCase()) !== -1) {
+          result.push(objects[i]);
+        }
+      }
+  }
+ 
+  return result;
+}
+
+render();
+
+//Создание модального окна
+const modal = document.getElementById("modal");
+modal.classList.add("modal");
+const modalBox = document.createElement("div");
+modalBox.classList.add("modal_box");
+
+//Кнопка закрытия
+const closeModalBtn = document.createElement("button");
+closeBtn = `<svg width="17" height="17" viewBox="0 0 17 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path fill-rule="evenodd" clip-rule="evenodd" d="M16.2332 1.73333L15.2665 0.766664L8.49985 7.53336L1.73318 0.766696L0.766515 1.73336L7.53318 8.50003L0.766542 15.2667L1.73321 16.2333L8.49985 9.46669L15.2665 16.2334L16.2332 15.2667L9.46651 8.50003L16.2332 1.73333Z" fill="#B0B0B0"/>
+</svg>`;
+closeModalBtn.innerHTML = closeBtn;
+closeModalBtn.classList.add("modal_close-btn");
+
+//Кнопка "Добавить клиента"
+const addClient = document.getElementById("addBtn");
+modal.append(modalBox);
+
+//Открытие модального окна
+addClient.addEventListener('click', function() {
+  modal.classList.add("open");
+  createNewClient();
+});
+
+//Закрытие по кнопке
+closeModalBtn.addEventListener('click', function() {
+  modal.classList.remove('open');
+  modalBox.innerHTML = "";
+})
+
+//Закрытие кликом вне окна
+modalBox.addEventListener('click', event => {
+  event._isClickWithInModal = true;
+}) 
+modal.addEventListener('click', event => {
+  if (event._isClickWithInModal) return;
+  event.currentTarget.classList.remove('open');
+  modalBox.innerHTML = "";
+})
+
+//Заполнение модального окна для создания нового клиента
+function createNewClient(clients) {
+  const titleModal = document.createElement("h2");
+  titleModal.textContent = "Новый клиент";
+  const inputs = document.createElement("div");
+  inputs.classList.add("inputs");
+
+  //Surename
+  const divSurename = document.createElement("div");
+  divSurename.classList.add("text-field");
+  const labelSurename = document.createElement("label");
+  labelSurename.classList.add("text-field__placeholder");
+  labelSurename.textContent = "Фамилия";
+  const inputSurename = document.createElement("input");
+  inputSurename.classList.add("text-field__inp");
+  inputSurename.type = "text";
+  inputSurename.required = true;
+  divSurename.append(inputSurename, labelSurename);
+  //Name
+  const divName = document.createElement("div");
+  divName.classList.add("text-field");
+  const labelName = document.createElement("label");
+  labelName.classList.add("text-field__placeholder");
+  labelName.textContent = "Имя";
+  const inputName = document.createElement("input");
+  inputName.classList.add("text-field__inp");
+  inputName.type = "text";
+  inputName.required = true;
+  divName.append(inputName, labelName);
+  //Lastname
+  const divLastname = document.createElement("div");
+  divLastname.classList.add("text-field");
+  const labelLastname = document.createElement("label");
+  labelLastname.classList.add("text-field__placeholder");
+  labelLastname.textContent = "Отчество";
+  const inputLastname = document.createElement("input");
+  inputLastname.classList.add("text-field__inp");
+  inputLastname.type = "text";
+  inputLastname.required = true;
+  divLastname.append(inputLastname, labelLastname);
+
+  if(clients) {
+    inputSurename.value = clients.surname;
+    inputName.value = clients.name;
+    inputLastname.value = clients.lastname;
+
+    const saveBtn = document.createElement("button");
+    saveBtn.textContent = "Сохранить";
+
+    const cancelBtn = document.createElement("button");
+    cancelBtn.textContent = "Удалить";
+
+  } else {
+    const saveBtn = document.createElement("button");
+    saveBtn.textContent = "Сохранить";
+
+    const cancelBtn = document.createElement("button");
+    cancelBtn.textContent = "Отмена";
+    cancelBtn.addEventListener("click", (e) => {
+      modal.classList.remove('open');
+      modalBox.innerHTML = "";
+    });
+  }
+
+  inputs.append(divSurename, divName, divLastname,);
+  modalBox.append(titleModal, inputs, saveBtn, cancelBtn, closeModalBtn);
+  modal.append(modalBox);
+}
